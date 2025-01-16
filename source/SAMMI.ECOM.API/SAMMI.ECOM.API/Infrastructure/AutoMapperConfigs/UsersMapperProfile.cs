96fb4214614d1cf2912113011d6ad5aa6c8a6b6f
hoantd <taduchoan5503@gmail.com>
2025-01-16 16:22:33 +0700

    update base

---
+++ source/SAMMI.ECOM.API/SAMMI.ECOM.API/Infrastructure/AutoMapperConfigs/UsersMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SAMMI.ECOM.Domain.AggregateModels.Others;
+using SAMMI.ECOM.Domain.DomainModels.Users;
+
+namespace SAMMI.ECOM.API.Infrastructure.AutoMapperConfigs
+{
+    public class UsersMapperProfile : Profile
+    {
+        public UsersMapperProfile()
+        {
+            CreateMap<User, EmployeeDTO>();
+        }
+    }
+}
---
+++ source/SAMMI.ECOM.API/SAMMI.ECOM.Infrastructure/Queries/UsersQueries.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using SAMMI.ECOM.Core.Models;
+using SAMMI.ECOM.Domain.AggregateModels.Others;
+using SAMMI.ECOM.Domain.DomainModels.Users;
+using SAMMI.ECOM.Repository.GenericRepositories;
+
+namespace SAMMI.ECOM.Infrastructure.Queries
+{
+    public interface IUsersQueries : IQueryRepository
+    {
+        Task<IEnumerable<EmployeeDTO>> GetEmployeeAll(RequestFilterModel? filterModel = null);
+    }
+    public class UsersQueries : QueryRepository<User>, IUsersQueries
+    {
+        public UsersQueries(SammiEcommerceContext context) : base(context)
+        {
+        }
+
+        public Task<IEnumerable<EmployeeDTO>> GetEmployeeAll(RequestFilterModel? filterModel = null)
+        {
+            return WithDefaultTemplateAsync(
+                (conn, sqlBuilder, sqlTemplate) =>
+                {
+                    sqlBuilder.Where("t1.Type = @type", new { type = "Employee" });
+                    return conn.QueryAsync<EmployeeDTO>(sqlTemplate.RawSql, sqlTemplate.Parameters);
+                }
+            );
+        }
+    }
+}

2c60ff21a6f6f53b72301ba27077581517410995
timmy277 <botono123@gmail.com>
2025-03-08 05:22:01 +0700

    feat: add brand, product, ..

--- source/FE/src/types/order/index.ts
+++ source/FE/src/types/order/index.ts
@@ -1,19 +1,33 @@
+import { ProductImage } from "../product";
 
 export type TParamsGetAllOrders = {
-    limit?: number,
-    page?: number,
-    search?: string,
-    order?: string
+    skip?: number;
+    take?: number;
+    filters?: string;
+    orderBy?: string;
+    dir?: string;
+    type?: number | (1 | 2 | 3 | 4 | 5 | 6)
+    paging?: boolean;
+    restrictOrderBy?: boolean;
+    keywords?: string;
 }
 
 export type TItemOrderProduct = {
-    name: string,
-    amount: number,
-    image: string,
-    price: number,
-    discount: number,
-    product: string,
-    slug: string
+    id: string;
+    code: string;
+    name: string;
+    stockQuantity: number;
+    price: number;
+    discount?: number;
+    ingredient: string;
+    uses: string;
+    usageGuide: string;
+    brandId: number;
+    categoryId: number;
+    status: number;
+    startDate?: string;
+    endDate?: string;
+    images: ProductImage[];
 }
 
 export type TItemOrderProductMe = {

3c021d40d057df54f4ddb0af6ccde25d675cf50b
timmy277 <botono123@gmail.com>
2025-03-09 07:04:17 +0700

    feat: add customer, supplier

--- source/FE/src/components/custom-select/index.tsx
+++ source/FE/src/components/custom-select/index.tsx
@@ -1,76 +1,79 @@
-import { Box, InputLabel, InputLabelProps, styled } from "@mui/material";
+import { Box, InputLabel, styled, FormControl } from "@mui/material";
 import Select, { SelectProps } from "@mui/material/Select";
-import MenuItem, { MenuItemProps } from '@mui/material/MenuItem';
+import MenuItem, { MenuItemProps } from "@mui/material/MenuItem";
 import { useTranslation } from "react-i18next";
 
 type TCustomSelect = SelectProps & {
-    options: { label: string, value: string }[]
-}
+  options: { label: string; value: string | number }[];
+};
 
 const StyledSelect = styled(Select)<SelectProps>(({ theme }) => ({
-    "& .MuiSelect-select.MuiSelect-outlined.MuiInputBase-input": {
-        padding: "8px 8px 8px 12px !important",
-        height: "38px",
-        boxSizing: "border-box"
-    },
-    "legend": {
-        display: "none"
-    },
-    ".MuiSvgIcon-root.MuiSvgIcon-fontSizeMedium": {
-        top: "calc(50% - .6em) !important"
-    },
-    ".MuiOutlinedInput-notchedOutline": {
-        top: "0px !important",
-        bottom: "2px !important",
-        height: "38px"
-    }   
-}))
+  "& .MuiSelect-select.MuiSelect-outlined.MuiInputBase-input": {
+    padding: "14px 8px 8px 12px !important", 
+    height: "38px",
+    boxSizing: "border-box",
+    display: "flex",        
+    alignItems: "center", 
+  },
+  "& .MuiOutlinedInput-notchedOutline": {
+    borderColor: theme.palette.grey[500],
+  },
+  "&:hover .MuiOutlinedInput-notchedOutline": {
+    borderColor: theme.palette.grey[700],
+  },
+  "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+    borderColor: theme.palette.primary.main,
+  },
+}));
 
-const CustomPlaceholder = styled(InputLabel)<InputLabelProps>(({ theme }) => ({
-    position: 'absolute',
-    top: "8px",
-    left: "10px",
-}))
-
-const StyledMenuItem = styled(MenuItem)<MenuItemProps>(({ theme }) => ({
-
-}))
+const StyledMenuItem = styled(MenuItem)<MenuItemProps>(({ theme }) => ({}));
 
 const CustomSelect = (props: TCustomSelect) => {
-    const { value, label, onChange, options, placeholder, fullWidth, ...rest } = props
-    const { t } = useTranslation()
-    return (
-        <Box sx={{
-            width: '100%',
-            height: '100%',
-            position: 'relative'
-        }}>
-            {((Array.isArray(value) && !value.length) || !value) && (
-                <CustomPlaceholder>{placeholder}</CustomPlaceholder>
-            )}
-            <StyledSelect
-                fullWidth={fullWidth}
-                labelId="demo-select-small-label"
-                id="demo-select-small"
-                value={value}
-                label={label}
-                onChange={onChange}
-                {...rest}
-            >
-                {options?.length > 0 ? options?.map((opt) => {
-                    return (
-                        <StyledMenuItem key={opt.value} value={opt.value}>
-                            {opt.label}
-                        </StyledMenuItem>
-                    )
-                }) : (
-                    <StyledMenuItem>
-                        {t('no_data')}
-                    </StyledMenuItem>
-                )}
-            </StyledSelect>
-        </Box>
-    )
-}
+  const { value, label, onChange, options, placeholder, fullWidth, ...rest } = props;
+  const { t } = useTranslation();
 
-export default CustomSelect
+  return (
+    <FormControl fullWidth={fullWidth}>
+      {label && (
+        <InputLabel
+          id={`custom-select-label-${label}`}
+          shrink={true} 
+          sx={{
+            transform: "translate(14px, -6px) scale(0.75)",
+            transformOrigin: "top left",
+          }}
+        >
+          {label}
+        </InputLabel>
+      )}
+      <StyledSelect
+        labelId={label ? `custom-select-label-${label}` : undefined}
+        fullWidth={fullWidth}
+        id="demo-select-small"
+        value={value}
+        label={label} 
+        size="medium"
+        onChange={onChange}
+        displayEmpty
+        {...rest}
+      >
+        {placeholder && !value && (
+          <StyledMenuItem value="" disabled>
+            <em>{placeholder}</em>
+          </StyledMenuItem>
+        )}
+        {options?.length > 0 ? (
+          options.map((opt) => (
+            <StyledMenuItem key={opt.value} value={opt.value}>
+              {opt.label}
+            </StyledMenuItem>
+          ))
+        ) : (
+          <StyledMenuItem disabled>{t("no_data")}</StyledMenuItem>
+        )}
+      </StyledSelect>
+    </FormControl>
+  );
+};
+
+export default CustomSelect;

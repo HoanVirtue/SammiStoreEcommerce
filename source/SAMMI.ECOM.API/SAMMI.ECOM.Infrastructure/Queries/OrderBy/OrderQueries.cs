95dfe2cf467c7acae6db3d4070b20116cda22d92
hoantd <taduchoan5503@gmail.com>
2025-03-30 20:10:25 +0700

    feat: fix_bug

--- source/SAMMI.ECOM.API/SAMMI.ECOM.Infrastructure/Queries/OrderBy/OrderQueries.cs
+++ source/SAMMI.ECOM.API/SAMMI.ECOM.Infrastructure/Queries/OrderBy/OrderQueries.cs
@@ -46,6 +46,40 @@ namespace SAMMI.ECOM.Infrastructure.Queries.OrderBy
             return WithPagingTemplateAsync(
                 (conn, sqlBuilder, sqlTemplate) =>
                 {
+                    sqlBuilder.Select("t2.FullName AS CustomerName, t2.Phone AS PhoneNumber");
+                    sqlBuilder.Select("CONCAT(t1.CustomerAddress, ', ', t4.Name, ', ', t5.Name, ', ', t6.Name) AS CustomerAddress");
+                    sqlBuilder.Select("SUM(t7.Quantity) AS TotalQuantity, SUM(t7.Quantity * t7.Price) AS TotalPrice");
+                    sqlBuilder.Select("t8.PaymentStatus");
+
+                    sqlBuilder.InnerJoin("Users t2 ON t1.CustomerId = t2.Id");
+                    sqlBuilder.LeftJoin("Voucher t3 ON t1.VoucherId = t3.Id");
+                    sqlBuilder.InnerJoin("Ward t4 ON t1.WardId = t4.Id");
+                    sqlBuilder.LeftJoin("District t5 ON t4.DistrictId = t5.Id");
+                    sqlBuilder.LeftJoin("Province t6 ON t5.ProvinceId = t5.Id");
+                    sqlBuilder.InnerJoin("OrderDetail t7 ON t1.Id = t7.OrderId");
+                    sqlBuilder.LeftJoin("Payment t8 ON t1.Id = t8.OrderId AND t8.IsDeleted != 1");
+
+                    sqlBuilder.GroupBy(@"t1.Id,
+                        t1.Code,
+                        t1.CustomerId,
+                        t1.OrderStatus,
+                        t1.ShippingStatus,
+                        t1.VoucherId,
+                        t1.WardId,
+                        t1.CustomerAddress,
+                        t1.CostShip,
+                        t1.TrackingNumber,
+                        t1.EstimatedDeliveryDate,
+                        t1.ActualDeliveryDate,
+                        t1.ShippingCompanyId,
+                        t1.CreatedDate,
+                        t1.UpdatedDate,
+                        t1.CreatedBy,
+                        t1.UpdatedBy,
+                        t1.IsActive,
+                        t1.IsDeleted,
+                        t1.DisplayOrder");
+                    sqlBuilder.GroupBy("t2.FullName, t2.Phone, t4.Name, t5.Name, t6.Name, t8.PaymentStatus");
                     return conn.QueryAsync<OrderDTO>(sqlTemplate.RawSql, sqlTemplate.Parameters);
                 },
                 filterModel);

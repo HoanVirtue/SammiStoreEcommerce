2a7727a94332f9e6b12a9e531c2c2e102e784ba9
taduchoan <taduchoan5503@gmail.com>
2025-04-24 09:07:59 +0700

    feat: update review

---
+++ source/SAMMI.ECOM.API/SAMMI.ECOM.API/Controllers/OrderBuy/ReviewsController.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SAMMI.ECOM.Infrastructure.Queries.OrderBy;
+using SAMMI.ECOM.Infrastructure.Repositories.OrderBy;
+
+namespace SAMMI.ECOM.API.Controllers.OrderBuy
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : CustomBaseController
+    {
+        private readonly IReviewQueries _reviewQueries;
+        private readonly IReviewRepository _reviewRepository;
+        public ReviewsController(
+            IReviewQueries reviewQueries,
+            IReviewRepository reviewRepository,
+            IMediator mediator,
+            ILogger<ReviewsController> logger) : base(mediator, logger)
+        {
+            _reviewQueries = reviewQueries;
+            _reviewRepository = reviewRepository;
+        }
+
+        public IActionResult Get()
+        {
+            return default;
+        }
+    }
+}

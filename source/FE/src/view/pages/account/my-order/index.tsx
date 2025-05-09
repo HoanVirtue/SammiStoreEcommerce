065598ea58e685e0a4a9d516f529038db1536d92
timmy277 <botono123@gmail.com>
2025-05-09 22:59:31 +0700

    feat: alter order

--- source/FE/src/view/pages/account/my-order/index.tsx
+++ source/FE/src/view/pages/account/my-order/index.tsx
@@ -86,7 +86,7 @@ const useOrderList = () => {
             take: pageSize,
             skip: (page - 1) * pageSize,
             paging: true,
-            orderBy: "updatedDate",
+            orderBy: selectedStatus === "Cancelled" ? "updatedDate" : "createdDate",
             dir: "desc",
             keywords: debouncedSearchTerm || "''",
             filters: selectedStatus === "all" ? "" : `orderStatus::${selectedStatus}::eq`
